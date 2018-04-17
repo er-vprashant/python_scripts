88f3eb90c729138054eca2c8a198765337b444a9
Prashant verma <xprashantxx@gmail.com>
2018-04-17 21:40:21 +0530

    Scientific Calculator
    Calculate any function using this cal 
    for example : sin(rad(90)) + 50% * (sqrt(16)) + round(1.42^2)
    
    functions supported by this Calculator
    
     +        : addition
     -        : subtraction
     *        : multiplication
     /        : division
     %        : percentage
     e        : 2.718281...
     pi       : 3.141592... 
     sin(rad) :sine
     cos(rad) :cosine
     tan(rad) :tangent
     XmodY    :remainder
     sqrt(n)  :square root  
     round(n) :round to nearest integer
     rad(deg) :convert degrees to radians

---
+++ calculator.py
@@ -0,0 +1,110 @@
+#Author: Prashant 
+#git: github.com/xprashantxx
+#mail: xprashantxx@gmail.com
+#Requirements: requests, BeautifulSoup
+
+'''Write your functions using the following
+--------------------------------------------
+ +        : addition
+ -        : subtraction
+ *        : multiplication
+ /        : division
+ %        : percentage
+ e        : 2.718281...
+ pi       : 3.141592... 
+ sin(rad) :sine
+ cos(rad) :cosine
+ tan(rad) :tangent
+ XmodY    :remainder
+ sqrt(n)  :square root  
+ round(n) :round to nearest integer
+ rad(deg) :convert degrees to radians
+'''
+
+import math
+import sys
+def calc(term):
+    """
+        input: term of type str
+        output: returns the result of the computed term.
+        purpose: This function is the actual calculator and the heart of the application
+    """
+    
+    # This part is for reading and converting arithmic terms.
+    term = term.replace(' ', '')
+    term = term.replace('^', '**')
+    term = term.replace('=', '')
+    term = term.replace('?', '')
+    term = term.replace('%', '/100')
+    term = term.replace('rad', 'radians')
+    term = term.replace('mod', '%')
+
+    functions = ['sin', 'cos', 'tan', 'cosh', 'sinh', 'tanh', 'sqrt', 'pi', 'radians', 'e'] 
+
+    # This part is for reading and converting function expressions.
+    for function in functions:            
+        if function in term.lower():
+            withmath = 'math.' + function
+            term = term.replace(function, withmath)
+
+    try:
+
+        # here goes the actual evaluating.
+        term = eval(term)
+
+    # here goes to the error cases.
+    except ZeroDivisionError:
+
+        print("Can't divide by 0")
+        exit(1) # exit(1) for indicating an error.
+
+    except NameError:
+
+        print('Invalid input')
+        exit(1)
+
+    except AttributeError:
+
+        print('Check usage method')
+        exit(1)
+        
+    return term
+
+
+def result(term):
+    """
+        input:  term of type str
+        output: none
+        purpose: passes the argument to the function calc(...) and 
+                prints the result onto console.
+    """
+    print("\n" + str(calc(term)))
+
+
+def main():
+    """
+        main-program
+        purpose: handles the user inputs and prints 
+                some informations onto console.
+    """
+    
+    print("\nScientific Calculator\n\nEg: sin(rad(90)) + 50% * (sqrt(16)) + round(1.42^2) - 12mod3\n\nEnter quit to exit")
+
+
+    if sys.version_info.major >= 3:
+        while True:
+            k = input("\nWhat is ")
+            if k == 'quit':
+                break
+            result(k)
+
+    else:
+        while True:
+            k = raw_input("\nWhat is ")
+            if k == 'quit':
+                break
+            result(k)
+
+
+if __name__ == '__main__':
+    main()
